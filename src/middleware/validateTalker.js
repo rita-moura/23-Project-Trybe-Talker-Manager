8d401f4531b3c8795200696c13d09123de97fb80
Rita Moura <ritamoura520@gmail.com>
2023-03-15 17:44:17 -0300

    Feat: Cria a rota /talker/:id com o verbo put, que permite editar uma pessoa usuária já existente

--- src/middleware/validateTalker.js
+++ src/middleware/validateTalker.js
@@ -1,6 +1,6 @@
 const isValidToken = (req, res, next) => {
   const { authorization } = req.headers;
-
+  
   if (!authorization) {
     return res.status(401).json({
       message: 'Token não encontrado',
--- src/router/talkerRouter.js
+++ src/router/talkerRouter.js
@@ -9,20 +9,20 @@ const talkerRouter = Router();
 const talkerPath = path.resolve(__dirname, '../talker.json');
 
 talkerRouter.get('/', async (_req, res) => {
-  const talkerRead = await readJsonData(talkerPath);
+  const talkers = await readJsonData(talkerPath);
 
-  if (!talkerRead) {
+  if (!talkers) {
     return res.status(200).json([]);
   }
 
-  return res.status(200).json(talkerRead);
+  return res.status(200).json(talkers);
 });
 
 talkerRouter.get('/:id', async (req, res) => {
-  const talkerRead = await readJsonData(talkerPath);
+  const talkers = await readJsonData(talkerPath);
   const { id } = req.params;
 
-  const talkerById = talkerRead.find((talker) => talker.id === Number(id));
+  const talkerById = talkers.find((talker) => talker.id === Number(id));
 
   if (!talkerById) {
     return res.status(404).json({
@@ -42,9 +42,30 @@ talkerRouter.post('/', ...validation, async (req, res) => {
       age,
       talk,
     };
-    const allTalker = JSON.stringify([...talkers, newTalker]);
-    await fs.writeFile(talkerPath, allTalker, null, 2);
+    const allTalker = JSON.stringify([...talkers, newTalker], null, 2);
+    await fs.writeFile(talkerPath, allTalker);
     res.status(201).json(newTalker);
+});
+
+talkerRouter.put('/:id', ...validation, async (req, res) => {
+  const talkers = await readJsonData(talkerPath);
+  const { name, age, talk } = req.body;
+  const { id } = req.params;
+  const talkerById = talkers.find((talker) => talker.id === Number(id));
+
+  if (!talkerById) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+  
+  const indexTalker = talkers.findIndex((talker) => talker.id === Number(id));
+  talkers[indexTalker] = { id: Number(id), name, age, talk };
+
+  const updatedtalkers = JSON.stringify(talkers, null, 2);
+  await fs.writeFile(talkerPath, updatedtalkers);
+
+  return res.status(200).json(talkers[indexTalker]);
 });
 
 module.exports = {
